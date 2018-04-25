ecdbcab4addb6c6a5ecf9e609f6aa5327c1f964e
Avent PC <Avent PC@RAVI>
2018-04-25 16:16:56 +0530

    update

---
+++ HelloWeb/src/com/test/controller/MainApp.java
@@ -0,0 +1,27 @@
+package com.test.controller;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+import com.test.model.HelloWorld;
+import com.test.model.JavaCollection;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		  //XmlBeanFactory factory = new XmlBeanFactory (new ClassPathResource("Beans.xml")); 
+	      //HelloWorld obj = (HelloWorld) factory.getBean("helloWorld");    
+	      //obj.getMessage();
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+	      JavaCollection jc=(JavaCollection)context.getBean("javaCollection");
+
+	      jc.getAddressList();
+	      jc.getAddressSet();
+	      jc.getAddressMap();
+	      jc.getAddressProp();
+	}
+
+}
---
+++ HelloWeb/src/com/test/model/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.test.model;
+
+public class HelloWorld {
+	
+	private String message;
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
