14518c826132da904ed22e2438ca190f61705d63
Avent PC <Avent PC@RAVI>
2018-05-10 17:30:23 +0530

    test

--- AngularJDemo/src/main/webapp/myscript.js
+++ AngularJDemo/src/main/webapp/myscript.js
@@ -13,7 +13,6 @@ app.controller("dataController", function($scope) {
 	var listEmployees = [
 			{firstName: "abc",lastName: "abc", salary:"20000"},
 			{firstName: "bbc",lastName: "bbc", salary:"30000"},
-			{firstName: "bbc",lastName: "bbc", salary:"10000"},
 			{firstName: "bbc",lastName: "bbc", salary:"40000"},
 			{firstName: "bbc",lastName: "bbc", salary:"80000"}
 	];
