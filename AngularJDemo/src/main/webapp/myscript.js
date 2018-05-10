a0c26814839f8039713de5f2bcc32ba3d9f58b77
Avent PC <Avent PC@RAVI>
2018-05-10 17:17:00 +0530

    added Angular Demo PROJ

---
+++ AngularJDemo/src/main/webapp/myscript.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+var app = angular.module("mymodule", []);
+
+app.controller("firstController", function($scope) {
+	$scope.message = "Hello controller";
+})
+
+app.controller("dataController", function($scope) {
+	
+	var listEmployees = [
+			{firstName: "abc",lastName: "abc", salary:"20000"},
+			{firstName: "bbc",lastName: "bbc", salary:"30000"},
+			{firstName: "bbc",lastName: "bbc", salary:"10000"}
+	];
+	
+	$scope.employess = listEmployees;
+})
