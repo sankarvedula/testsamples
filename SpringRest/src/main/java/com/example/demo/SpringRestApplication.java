9ef44358daae810b8a7108563fedade66ed6b728
sankarvedula <ravi.sankar14@gmail.com>
2018-07-04 19:03:50 +0530

    Update SpringRestApplication.java

--- SpringRest/src/main/java/com/example/demo/SpringRestApplication.java
+++ SpringRest/src/main/java/com/example/demo/SpringRestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringRestApplication {
-
+/** Test comment **/
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApplication.class, args);
 	}
